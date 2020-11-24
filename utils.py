46e25998c5ae342061d10ca278b1f78e15074de1
ningchaoar <ningchao8358@gmail.com>
2020-11-24 14:55:36 +0800

    预测结果评估

--- utils.py
+++ utils.py
@@ -9,6 +9,8 @@ import logging
 import numpy as np
 from typing import *
 from tqdm import tqdm
+from sklearn import metrics
+
 from category import Category
 
 
@@ -104,3 +106,23 @@ def load_model(model_dir: str):
         labels = [line.strip() for line in fr]
 
     return vocabulary, p_c, p_w_c, labels
+
+
+def get_report(result_file: str, label_file: str):
+    with open(label_file, "r", encoding="utf-8") as fr:
+        labels = [line.strip() for line in fr]
+    with open(result_file, "r", encoding="utf-8") as fr:
+        fr = [line.strip().rsplit("\t", 1) for line in fr]
+    y_predict = []
+    y_label = []
+    for line in tqdm(fr):
+        raw_data, y = line
+        label = raw_data.split("_!_")[2]
+        y_predict.append(labels.index(y))
+        y_label.append(labels.index(label))
+    print(metrics.classification_report(y_label, y_predict, target_names=labels))
+
+
+if __name__ == "__main__":
+    get_report(result_file="resources/cropus/toutiao_cat_data_result.txt",
+               label_file="resources/model/toutiao_news_model/labels.txt")
